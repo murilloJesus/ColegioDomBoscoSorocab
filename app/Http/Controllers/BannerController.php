a52c52e960f4590c572e580f5a7fd42385b95451
Murillo Jesus <murillosjesus@hotmail.com>
2022-05-23 11:19:04 -0300

    resolve home Banners

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -116,6 +116,6 @@ class BannerController extends Controller
      */
     static function getFromJSON()
     {
-        return Json::decode( Storage::get("/data/banners.json") );
+        return Json::decode( Storage::get("/data/banners.json") ? Storage::get("/data/banners.json") : [] );
     }
 }
