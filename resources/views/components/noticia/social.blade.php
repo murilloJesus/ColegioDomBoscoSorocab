2a2caca24fe7ff9004adefe0b78e11a7120af71d
Murillo Jesus <murillosjesus@hotmail.com>
2022-05-23 15:59:00 -0300

    resolve Noticias

--- resources/views/components/noticia/social.blade.php
+++ resources/views/components/noticia/social.blade.php
@@ -1,6 +1,7 @@
 <ul class="icons">
-    <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-    <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-    <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-    <li><a href="#" class="icon brands fa-whatsapp"><span class="label">WhatsApp</span></a></li>
+    <li><a href="https://www.facebook.com/domboscosorocaba/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+    <li><a href="https://www.instagram.com/domboscosorocaba/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+    <li><a href="https://www.youtube.com/c/DomBoscoSorocaba" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
+    <li><a href="https://www.linkedin.com/company/colegiodombosco/" class="icon brands fa-linkedin"><span class="label">Linked In</span></a></li>
+    <li><a href="https://api.whatsapp.com/send?phone=5515974025810&text=" class="icon brands fa-whatsapp"><span class="label">WhatsApp</span></a></li>
 </ul>
