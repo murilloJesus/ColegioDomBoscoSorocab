a8a7066e7e306a57e78efc0ae17dfdc14fc1d57d
Murillo Jesus <murillosjesus@hotmail.com>
2022-05-23 10:11:12 -0300

    resolve galleria upload

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -55,8 +55,8 @@ class GalleryController extends Controller
         $file = $request->file->storeAs('zips', $request->file('file')->getClientOriginalName()); // OK
 
         $zip = new ZipArchive;
-        $res = $zip->open($file, ZipArchive::CREATE | ZIPARCHIVE::OVERWRITE);
-        if($res === TRUE){
+        $res = $zip->open(storage_path("app/$file"), ZipArchive::CREATE | ZIPARCHIVE::OVERWRITE);
+        if($res === TRUE){// OK
             $galeria =  $zip->extractTo(storage_path("app$this->private_path"));
             if($galeria){
                 $this->saveJSON();
